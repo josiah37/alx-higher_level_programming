0d142cc00457ac590adba179d2be24bdeadb76bc
josiah37 <eyosiyasdemsew@gmail.com>
2024-01-07 15:30:04 +0300

    try2

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+
+    # importing only the add func
     from add_0 import add
     a = 1
     b = 2
-    c = add(a, b)
-    #if __name__ == "__main__":
-    print(f"{a} + {b} = {c}") 
-
+    # print(f"{a} + {b} = {add(a, b)}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
