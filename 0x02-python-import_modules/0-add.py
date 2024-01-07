efdc3868d260ab28b1afe69a2eea9c591041b565
josiah37 <eyosiyasdemsew@gmail.com>
2024-01-07 14:15:20 +0300

    modifing task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,7 +3,5 @@ from add_0 import add
 a = 1
 b = 2
 c = add(a, b)
-#if __name__ == "__main__":
-	print(f"the dendenr name __name__ {__name__} ")
-	print(f"{a} + {b} = {c}") 
-
+if __name__ == "__main__":
+    print(f"{a} + {b} = {c}")
