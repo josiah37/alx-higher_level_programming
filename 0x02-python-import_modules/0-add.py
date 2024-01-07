190a6021c969e9d1cf41b1199596c9cf09d2dbce
josiah37 <eyosiyasdemsew@gmail.com>
2024-01-07 14:06:33 +0300

    this is my p commit for 0 add that is task 1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+from add_0 import add
+a = 1
+b = 2
+c = add(a, b)
+#if __name__ == "__main__":
+	print(f"the dendenr name __name__ {__name__} ")
+	print(f"{a} + {b} = {c}") 
 
-if __name__ == "__main__":
-    """Print the sum of 1 and 2."""
-    from add_0 import add
-
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
