95224ac1bf4d59c7b623123833813f261e3e849f
josiah37 <eyosiyasdemsew@gmail.com>
2024-01-09 23:08:24 +0300

    modifying task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
+
+"""
+* If idx is negative, the function should not modify anything,
+  and returns the original list
+
+* If idx is out of range (> of number of element in my_list),
+  the function should not modify anything, and returns the original list
+
+
+               white boarding
+list[wz indx to be changed] = the new element
+"""
+
+
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list) - 1:
+    if idx < 0 or idx > (len(my_list)):
         return my_list
     else:
         my_list[idx] = element
