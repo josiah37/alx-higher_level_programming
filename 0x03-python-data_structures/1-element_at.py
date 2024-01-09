7bc80fab6e08e058083e3690018c0146bf88491a
josiah37 <eyosiyasdemsew@gmail.com>
2024-01-09 18:30:49 +0300

    now it is easy

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list) - 1:
-        return 'None'
+    # i replaced the 2 if conditons to wz 1 as the excution is the same
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
     else:
         return my_list[idx]
