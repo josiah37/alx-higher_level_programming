5ad7c5726106be9e2fe6838002bd9118b195d2c2
josiah37 <eyosiyasdemsew@gmail.com>
2024-04-24 02:04:11 +0300

    adding the __str__ again

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -94,7 +94,8 @@ class Rectangle(Base):
             print("")
 
     def __str__(self):
-        return (f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}")
+        return (f"[Rectangle] ({self.id}) {self.x}/{self.y} - \
+{self.width}/{self.height}")
 
     """
     def update(self, *args):
